d9cde900fe3d438988b59f17a8ed548aaff6842f
jim@jimdonaghy.com <jim@jimdonaghy.com>
2023-05-21 11:40:19 -0700

    updates

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,70 @@
 <script>
-	export let name;
+
+	
+	const formatter = new Intl.NumberFormat('en-US', {
+		style: 'currency',
+		currency: 'USD',
+		minimumFractionDigits: 2
+	})
+
+
+	let promise = fetch('http://localhost:8083/homedata').then((x) => x.json());
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+{#await promise}
+	<!-- optionally show something while promise is pending -->
+{:then data}
+	<!-- promise was fulfilled -->
+
+	<div class="container">
+		<h1>Bank Account Summary</h1>
+		<p>Account Holder: {data.customer.name} | {data.customer.address} | {data.customer.phone}</p>
+		<p>Account Number: {data.account.accountNumber}</p>
+		<p>Current Balance ${data.account.balance}.00</p>
+	
+		<table>
+		  <tr>
+			<th>Date</th>
+			<th>Description</th>
+			<th>Amount</th>
+		</tr>
+		  {#each data.account.transactions as transaction}
+		  <tr>
+			<td>{transaction.transactionDate}</td>
+			<td>{transaction.description}</td>
+			<td>${transaction.amount}.00</td>
+		  </tr>
+		  {/each}
+		</table>
+	  </div>
+
+{:catch error}
+	<!-- optionally show something while promise was rejected -->
+{/await}
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    body {
+      font-family: Arial, sans-serif;
+    }
+    .container {
+      max-width: 800px;
+      margin: 0 auto;
+    }
+    table {
+      width: 100%;
+      margin-top: 20px;
+    }
+    th, td {
+      text-align: left;
+      padding: 8px;
+    }
+    th {
+      background-color: #4CAF50;
+      color: white;
+    }
+    tr:nth-child(even) {
+      background-color: #f2f2f2;
+    }
+  </style>
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
---
+++ src/store.js
@@ -0,0 +1,16 @@
+import { writable, derived } from 'svelte/store';
+
+/** Store for response data. 
+**/
+export const apiData = writable({});
+
+/** Data transformation.
+**/
+export const customer = {
+  
+ 
+  //if ($apiData.drinks){
+  //  return $apiData.drinks.map(drink => drink.strDrink);
+  //}
+  //return [];
+};
